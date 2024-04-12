538a16d9e13824165a1766f98b1c1c9a74b36b48
hayashi8953 <166467853+hayashi8953@users.noreply.github.com>
2024-04-12 09:17:40 +0900

    Update Messages.cs
    コンストラクタの追加

--- BlazorGroupB/Models/Messages.cs
+++ BlazorGroupB/Models/Messages.cs
@@ -13,6 +13,21 @@ public class Messages
     public int MessageNumber { get; set; }
     public DateTime PostTime { get; set; }
 
-    
+    //  空のコンストラクタと値を得るコンストラクタ
+    public Messages()
+    {
+
+    }
+    public Messages(int messageId, int threadId, string userId, string writeName, 
+        string detailMessage, int messageNumber, DateTime postTime)
+    {
+        this.MessageID = messageId;
+        this.ThreadId = threadId;
+        this.UserID = userId;
+        this.WriteName = writeName;
+        this.DetailMessage = detailMessage;
+        this.MessageNumber = messageNumber;
+        this.PostTime = postTime;
+    }
 
 }
