3dfe810f6e4c58639a53f655570b5eeaaa37f26c
hayashi8953 <166467853+hayashi8953@users.noreply.github.com>
2024-04-12 12:55:27 +0900

    Update Messages.cs
    masterに送ったものと同様の変更を再度送っています

--- BlazorGroupB/Models/Messages.cs
+++ BlazorGroupB/Models/Messages.cs
@@ -6,13 +6,28 @@ namespace BlazorGroupB.Models;
 public class Messages
 {
     public int MessageID { get; set; }
-    public int ThreadId { get; set; }
+    public int ThreadID { get; set; }
     public string? UserID { get; set; }
     public string WriteName { get; set; }
     public string? DetailMessage { get; set; }
     public int MessageNumber { get; set; }
     public DateTime PostTime { get; set; }
 
-    
+    //  空のコンストラクタと値を得るコンストラクタ
+    public Messages()
+    {
+
+    }
+    public Messages(int messageId, int threadId, string userId, string writeName, 
+        string detailMessage, int messageNumber, DateTime postTime)
+    {
+        this.MessageID = messageId;
+        this.ThreadId = threadId;
+        this.UserID = userId;
+        this.WriteName = writeName;
+        this.DetailMessage = detailMessage;
+        this.MessageNumber = messageNumber;
+        this.PostTime = postTime;
+    }
 
 }
